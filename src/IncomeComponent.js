e670aa2733b10b68f32abe2148f753bfabf40e21
Quinn Riley <qriley@oswego.edu>
2022-11-15 18:17:05 -0500

    -Added total display at bottom -Fixed input handling in IncomeComponent

--- src/IncomeComponent.js
+++ src/IncomeComponent.js
@@ -1,30 +1,36 @@
 import React from 'react';
 
 class IncomeComponent extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-        income: 0
-     };
-  }
-  myIncomeHandler = (event) => {
-    this.setState({income: event.target.value});
-  }
-  render() {
-    return (
-    <div className="incomeCard">
-      <form>
-      <h1>Determine your monthly income:</h1>
-      <p>Enter your monthly income after taxes:</p>
-      <input
-        type='text'
-        onChange={this.myIncomeHandler}
-      />
+    constructor(props) {
+        super(props);
+        this.state = {
+            income: 0
+        };
+    }
 
-      </form>
-    </div>
-    );
-  }
+    myIncomeHandler = (event) => {
+        event.preventDefault();
+        this.setState({ income: event.target.value });
+
+        
+        document.getElementsByClassName("incomeTotalDisplay")[0].innerHTML = this.state.income;
+
+    }
+    render() {
+        return (
+            <div className="incomeCard">
+                <form>
+                    <h1>Determine your monthly income:</h1>
+                    <p>Enter your monthly income after taxes:</p>
+                    <input
+                        type='text'
+                        onChange={this.myIncomeHandler}
+                    />
+                    <button onClick={this.myIncomeHandler}>Confirm</button>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default IncomeComponent;
