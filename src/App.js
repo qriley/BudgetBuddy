ba298952d5c352935a7c432dff557a0ce3a34e4d
Quinn Riley <qriley@oswego.edu>
2022-11-23 00:05:53 -0500

    Styling Update and prepping for investment and money management stragegies to choose from.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ class App extends Component {
         }
 
     }
-    //const [components, setComponents] = useState([]);
     updateTotal = (toAdd) => {
         console.log(toAdd)
         console.log(this.state.total)
@@ -27,9 +26,9 @@ class App extends Component {
         this.setState({ components: this.state.components++ });
         var tempRows = this.state.rows
 
-        
-            
-            // note: we are adding a key prop here to allow react to uniquely identify each
+
+
+        // note: we are adding a key prop here to allow react to uniquely identify each
         // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
         tempRows.push(<ExpenseComp key={this.components} totalFunction={this.updateTotal} />);
 
@@ -39,27 +38,43 @@ class App extends Component {
 
     render() {
         return (
-        <div className="App">
-      <header className="App-header">
-        <h1>Budget Buddy</h1>
-        <h3>...to help your finances!</h3>
-      </header>
-      <div className="mainContent">
-        <IncomeComp />
-                    <div className="additionalCards">
-                        {this.state.rows}
-                  
-              </div>
-              <button onClick={this.addComponent}>Add Expense</button>
+            <div className="App">
+                <header className="App-header">
+                    <h1>Budget Buddy</h1>
+                    <h3>...to help your finances!</h3>
+                </header>
+                <div className="mainContent">
+                    <div className="leftCol">
+                        <IncomeComp />
+                        <div className="additionalCards">
+                            {this.state.rows}
 
-              <div className="incomeTotalCard">
-                  <h1 className="incomeTotalDisplay">${ this.state.total}</h1>
-               </div>
-      </div>
-    </div>
-            );
+                        </div>
+                        <button onClick={this.addComponent}>Add Expense</button>
+
+                        <div className="incomeTotalCard">
+                            <h1 className="incomeTotalDisplay">${this.state.total}</h1>
+                        </div>
+                    </div>
+                    <div className="rightCol">
+                        <button className="stratCard">
+                            TEST
+                        </button>
+                        <button className="stratCard">
+                            TEST
+                        </button>
+                        <button className="stratCard">
+                            TEST
+                        </button>
+                        <button className="stratCard">
+                            TEST
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
     }
-    
+
 }
 
 export default App;
